78f97feaf1fe83fcc257a3778b86eea7ea8010f5
steve02081504 <steve02081504@EDEN.fukwold>
2026-09-05 14:16:59 +0800

    update

--- main.mjs
+++ main.mjs
@@ -2,9 +2,18 @@ import process from 'node:process'
 
 const shutdown_functions = []
 export function on_shutdown(func) { shutdown_functions.unshift(func) }
-let error_handler = console.error
+let error_handler = (...args) => console.error(...args) // allows libs like @steve02081504/virtual-console to override the default console.error behavior
 export function on_shutdown_error(func) { error_handler = func }
 const exit = process.exit
+
+async function handle_error(error) {
+	try { await error_handler(error) }
+	catch (error) {
+		try { await error_handler(error) } // you silly
+		catch (error) { console.error(error) } // wtf bro?
+	}
+}
+
 let is_shutting_down
 export async function shutdown(code) {
 	if (is_shutting_down) return
@@ -13,11 +22,7 @@ export async function shutdown(code) {
 	for (const func of shutdown_functions) try {
 		await func(code)
 	} catch (error) {
-		try { await error_handler(error) }
-		catch (error) {
-			try { await error_handler(error) } // you silly
-			catch (error) { console.error(error) } // wtf bro?
-		}
+		await handle_error(error)
 	}
 	exit(code)
 }
@@ -26,15 +31,17 @@ export function shutdown_with(...additional_fns_or_code) {
 	let code
 	if (Object(additional_fns_or_code.slice(-1)[0] ?? 0) instanceof Number) code = additional_fns_or_code.pop()
 	return async _ => {
-		for (const func of additional_fns_or_code) try { await func() } catch (error) { console.error(error) }
+		for (const func of additional_fns_or_code) try { await func() } catch (error) { await handle_error(error) }
 		await shutdown(code)
 	}
 }
 
 export const shutdown_listeners = {}
-export function set_shutdown_listener(event, code, event_fn) {
-	unset_shutdown_listener(event)
-	process.on(event, shutdown_listeners[event] = shutdown_with(() => event_fn?.(event), code))
+export function set_shutdown_listener(events, code, event_fn) {
+	for (const event of Array.isArray(events) ? events : [events]) {
+		unset_shutdown_listener(event)
+		process.on(event, shutdown_listeners[event] = shutdown_with(() => event_fn?.(event), code))
+	}
 }
 export function unset_shutdown_listener(...events) {
 	for (const event of events) {
@@ -47,7 +54,5 @@ export function unset_shutdown_listener(...events) {
 set_shutdown_listener('SIGINT', 130)
 set_shutdown_listener('SIGTERM', 143)
 set_shutdown_listener('SIGHUP', 0)
-set_shutdown_listener('error', 1, console.error)
-set_shutdown_listener('uncaughtException', 1, console.error)
-set_shutdown_listener('unhandledRejection', 1, console.error)
+set_shutdown_listener(['error', 'uncaughtException', 'unhandledRejection'], 1, error_handler)
 set_shutdown_listener('beforeExit')
